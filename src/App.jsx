89959999f33852a53152a168cb59a0c7977ea9a3
sanket-pilane <lalitpilane7@gmail.com>
2023-10-07 11:24:13 +0530

    Project Started

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PlayGame from "./components/PlayGame";
+
+const App = () => {
+  return <PlayGame />;
+};
+
+export default App;
---
+++ src/components/PlayGame.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+const PlayGame = () => {
+  return <Button>This is a Button</Button>;
+};
+
+export default PlayGame;
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px 20px;
+`;
